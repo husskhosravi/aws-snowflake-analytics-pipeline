ce060ac2b4380bb9ac736c8e14ccc544a5c13f83
Huss Khosravi <huss.khosravi@gmail.com>
2025-04-30 23:44:38 +1000

    Update sentiment_udf.py

--- udf/sentiment_udf.py
+++ udf/sentiment_udf.py
@@ -1,1 +1,10 @@
+from textblob import TextBlob
 
+def analyze_sentiment(text):
+    polarity = TextBlob(text).sentiment.polarity
+    if polarity > 0:
+        return 'positive'
+    elif polarity == 0:
+        return 'neutral'
+    else:
+        return 'negative'
